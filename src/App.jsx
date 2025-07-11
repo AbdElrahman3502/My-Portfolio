b652667980e28fd60b92644c71c895daec73bc6e
AbdElrahman3502 <eabd4131@gmail.com>
2025-07-11 10:29:18 +0300

    Upload project files

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <>
+            <h1 className={"text-center text-3xl font-bold text-yellow-500 underline"}> Hello World ! </h1>
+        </>
+    );
+};
